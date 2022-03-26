1ad4a35d3df52207d767220412dbd67a207c8c72
Ugo Chux <ugochux@Ugos-MacBook-Pro.local>
2022-03-26 11:08:15 +0200

    made changes to myContribution

--- game.py
+++ game.py
@@ -4,13 +4,13 @@ attempts_list = []
 def show_score():
     #Define score using if and else statements
     if len(attempts_list) <= 0:
-        print("No high score at the moment")
+        print("No high score at the moment, grab this opportunity")
     else:
         print("For now, the high score is {} attempts ".format(min(attempts_list)))
 def start_game():
     random_number = int(random.randint(1, 10))
     print("Hello traveler! Welcome to the game of guesses!")
-    player_name = input("What is your name? ")
+    player_name = input("What is your name ? ")
     wanna_play = input("Hi, {}, would you like to play the guessing game? (Enter Yes/No) ".format(player_name))
     #// Where the show_score function USED to be
     attempts = 0
