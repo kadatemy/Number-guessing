1af334dcb1c3e3128a37f8f5b913efc900283b1a
Nwachinemere7 <titus.chukwuemeka07@gmail.com>
2022-03-27 17:24:51 +0100

    This is a new Addition

--- game.py
+++ game.py
@@ -3,41 +3,46 @@ import random
 attempts_list = []
 def show_score():
     #Define score using if and else statements
-    def start_game():
-        start_game()
-random_number = int(random.randint(1, 10))
-print("Hello traveler! Welcome to the game of guesses!")
-    
-    
-player_name = input("What is your name? ")
-wanna_play = input("Hi, {}, would you like to play the guessing game? (Enter Yes/No) ".format(player_name))
-show_score()
-attempts = 0
-while wanna_play.lower() == "yes":
-    try:
-                    #Guess is between 1 - 20
-        guess = input("Enter a number between 1 - 20: ")
-        if int(guess) == random_number:
-            print("Nice! You got it!")
-            attempts += 1
-            attempts_list.append(attempts)
-            print("It took you {} attempts".format(attempts))
-            play_again = input("Would you like to play again? (Enter Yes/No) ")
-            attempts = 0
-            show_score()
-            random_number = int(random.randint(1, 20))
-        elif play_again.lower() == "no":
-            print("That's cool, have a good one!")
-            break
-        else:
-                print("Welcome!")
-                continue
-                    #Write elif statements for when the number is lower or higher with print statements, increment attempts
-    except ValueError as err:
-        print("Oh no!, that is not a valid value. Try again...")
-        print("({})".format(err))
+    if len(attempts_list) <= 0:
+        print("No high score at the moment, so take it!")
+    else:
+        print("The high score is {} attempts".format(min(attempts_list)))
 
+def start_game():
+    random_number = int(random.randint(1, 20))
+    print("Hello traveler! Welcome to the game of guesses!")
+    player_name = input("What is your name? ")
+    wanna_play = input("Hi, {}, would you like to play the guessing game? (Enter Yes/No) ".format(player_name))
+    # Where the show_score function USED to be
+    attempts = 0
+    show_score()
+    while wanna_play.lower() == "yes":
+        try:
+            #Guess is between 1 - 20
+            guess = input("Pick a number between 1 and 20 ")
+            if int(guess) == random_number:
+                print("Nice! You got it!")
+                attempts += 1
+                attempts_list.append(attempts)
+                print("It took you {} attempts".format(attempts))
+                play_again = input("Would you like to play again? (Enter Yes/No) ")
+                attempts = 0
+                show_score()
+                random_number = int(random.randint(1, 20))
+                if play_again.lower() == "no":
+                    print("That's cool, have a good one!")
+                    break
+            #Write elif statements for when the number is lower or higher with print statements, increment attempts
+            elif int(guess) > random_number:
+                print("It's lower")
+                attempts += 1
+            elif int(guess) < random_number:
+                print("It's higher")
+                attempts += 1
+        except ValueError as err:
+            print("Oh no!, that is not a valid value. Try again...")
+            print("({})".format(err))
     else:
         print("That's cool, have a good one!")
-        if __name__ == '__main__': 
-start_game()
+if __name__ == '__main__':
+    start_game()
